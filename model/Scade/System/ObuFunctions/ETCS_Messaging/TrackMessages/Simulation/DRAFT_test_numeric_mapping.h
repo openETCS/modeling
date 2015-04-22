7622acc8245518a6fb71ce99e46331b55cd86225
JakobGartner <j.gaertner@railergy.com>
2015-04-22 03:38:26 +0200

    Added read support for packet 3 national values
    using legacy type for now
    with known limitations

---
+++ model/Scade/System/ObuFunctions/ETCS_Messaging/TrackMessages/Simulation/DRAFT_test_numeric_mapping.h
@@ -0,0 +1,15 @@
+#ifndef DRAFT_TEST_NUMERIC_SCSIM_MAPPING
+#define DRAFT_TEST_NUMERIC_SCSIM_MAPPING
+
+#include "SmuTypes.h"
+#include "DRAFT_test_numeric_type.h"
+
+void _SCSIM_Mapping_Create();
+static ControlUtils _SCSIM_BoolEntity_Control_Utils;
+#include "DRAFT_test_numeric.h"
+void _SCSIM_Mapping_DRAFT_test_numeric();
+
+void* _SCSIM_Get_DRAFT_test_numeric_Handle(void* pInstance, int nHandleIdent, int* pIteratorFilter, int nSize);
+
+
+#endif /*DRAFT_TEST_NUMERIC_SCSIM_MAPPING */
--- model/Scade/System/ObuFunctions/ETCS_Messaging/TrackMessages/Simulation/kcg_sensors.h
+++ model/Scade/System/ObuFunctions/ETCS_Messaging/TrackMessages/Simulation/kcg_sensors.h
@@ -1,6 +1,6 @@
 /* $*************** KCG Version 6.1.3 (build i6) ****************
 ** Command: s2c613 -config C:/GITHUB/modeling/model/Scade/System/ObuFunctions/ETCS_Messaging/TrackMessages/Simulation\kcg_s2c_config.txt
-** Generation date: 2015-04-02T18:32:17
+** Generation date: 2015-04-22T00:28:15
 *************************************************************$ */
 #ifndef _KCG_SENSORS_H_
 #define _KCG_SENSORS_H_
@@ -10,6 +10,6 @@
 #endif /* _KCG_SENSORS_H_ */
 /* $*************** KCG Version 6.1.3 (build i6) ****************
 ** kcg_sensors.h
-** Generation date: 2015-04-02T18:32:17
+** Generation date: 2015-04-22T00:28:15
 *************************************************************$ */
 
