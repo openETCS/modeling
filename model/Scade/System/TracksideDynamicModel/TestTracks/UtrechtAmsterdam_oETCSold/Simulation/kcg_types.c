a23b01dbe882aad5e69f5e9206b8be311a4c2c31
JakobGartner <j.gaertner@railergy.com>
2015-04-23 23:04:52 +0200

    New track model
    Packet 5 sent from BG364
    linked BGs 365.366.367.368.369.341

---
+++ model/Scade/System/TracksideDynamicModel/TestTracks/UtrechtAmsterdam_oETCSold/Simulation/kcg_types.c
@@ -0,0 +1,12 @@
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** Command: s2c613 -config C:/GITHUB/modeling/model/Scade/System/OBU_for_ITEA-Review/TestTracks/UtrechtAmsterdam_oETCS/Simulation\kcg_s2c_config.txt
+** Generation date: 2015-03-04T20:08:57
+*************************************************************$ */
+
+#include "kcg_types.h"
+
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** kcg_types.c
+** Generation date: 2015-03-04T20:08:57
+*************************************************************$ */
+
