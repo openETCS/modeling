e8f30490976fb812fa7fe48c780c27f32b88cc1d
JakobGartner <j.gaertner@railergy.com>
2015-04-28 09:22:11 +0200

    Balise Lib updated
    contains now support for track discontinuities
    
    - A track topology consists of more than one section
    - Each section may have its own coordinate system for engineering data
    - We now have an operator to drive a train across a track layout with
    different sections.
    - Each balise knows its nominal position (pos) in its local coordinate
    system
    - The train only knows his own position (TrainPos) which is strictly
    linear

---
+++ model/Scade/System/BaliseLib/Simulation/kcg_consts.c
@@ -0,0 +1,12 @@
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** Command: s2c613 -config C:/GITHUB/modeling/model/Scade/System/TracksideDynamicModel/BaliseLib/Simulation\kcg_s2c_config.txt
+** Generation date: 2015-04-27T21:06:24
+*************************************************************$ */
+
+#include "kcg_consts.h"
+
+/* $*************** KCG Version 6.1.3 (build i6) ****************
+** kcg_consts.c
+** Generation date: 2015-04-27T21:06:24
+*************************************************************$ */
+
