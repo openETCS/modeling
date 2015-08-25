9c46f3145b5a428718fba64fd2f57a28871560d7
Johannes Kastner <Johannes.Kastner@ics-ag.de>
2015-08-25 18:58:16 +0200

    Test framework: add packet21 to msg.tcl (#684)

---
+++ test/src/lib/const.tcl
@@ -0,0 +1,15 @@
+# Provides commonly used constants (in particular ETCS enumeration values)
+#
+# History:
+# - 25.08.15: J. Kastner: initial version
+
+set Q_DIR_Reverse 0
+set Q_DIR_Nominal 1
+set Q_DIR_Both    2
+
+set Q_GDIR_downhill 0
+set Q_GDIR_uphill   1
+
+set Q_SCALE_10cm 0
+set Q_SCALE_1m   1
+set Q_SCALE_10m  2
