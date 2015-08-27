460a5cf99cd9920fb6e1a19bb8c173b8f93efcd2
Johannes Kastner <Johannes.Kastner@ics-ag.de>
2015-08-27 10:10:53 +0200

    Tests: add test script for TrackAtlas/Build_MA

--- test/src/TrackAtlas/MovementAuthority/ma.tcl
+++ test/src/TrackAtlas/MovementAuthority/ma.tcl
@@ -6,5 +6,16 @@ source ../../lib/const.tcl
 
 proc checkMA {flag args} {
   util::check "$::eut/" "new_MA=$flag"
-  eval util::check "$::eut/MA_onboard_out." $args
+  foreach arg $args {
+    switch -glob $arg {
+      section* {
+        set t [split $arg :]
+        set sectionId [string range [lindex $t 0] 7 8]
+        util::log $sectionId
+        set data [split [lindex $t 1] ,]
+        eval util::check {"$::eut/MA_onboard_out.sections\[$sectionId\]."} $data
+      }
+      default { util::check "$::eut/MA_onboard_out." $arg }
+    }
+  }
 }
