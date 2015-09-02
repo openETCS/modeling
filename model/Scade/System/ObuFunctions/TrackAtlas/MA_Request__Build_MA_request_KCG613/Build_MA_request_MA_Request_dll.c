d8eb9c9014391cd7985a08934156d5ddaa4676d5
JakobGartner <j.gaertner@railergy.com>
2015-09-02 02:16:32 +0200

    Track Atlas update
    #708

--- model/Scade/System/ObuFunctions/TrackAtlas/MA_Request__Build_MA_request_KCG613/Build_MA_request_MA_Request_dll.c
+++
@@ -1,4 +0,0 @@
-const char*  pszDllPathname = "MA_Request__Build_MA_request.dll";
-
-const char*  pszLauncherPathname = "C:/program files/esterel technologies/scade r16.1/scade/bin/SCSSMLNC.exe";
-
--- model/Scade/System/ObuFunctions/TrackAtlas/Simulation/TrackAtlasNextGen_TrackAtlas_dll.c
+++
@@ -1,4 +0,0 @@
-const char*  pszDllPathname = "TrackAtlas__TrackAtlasNextGen.dll";
-
-const char*  pszLauncherPathname = "C:/Program Files/Esterel Technologies/SCADE R16.1/SCADE/bin/SCSSMLNC.exe";
-
