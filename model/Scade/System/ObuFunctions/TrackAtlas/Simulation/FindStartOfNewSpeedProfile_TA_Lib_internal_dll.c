04f53ac6c10f39b3d0d73a2cf3cc5f21c86bfb5b
JakobGartner <j.gaertner@railergy.com>
2015-09-03 19:09:41 +0200

    Track Atlas maturing: SSP now renovated

--- model/Scade/System/ObuFunctions/TrackAtlas/Simulation/FindStartOfNewSpeedProfile_TA_Lib_internal_dll.c
+++ model/Scade/System/ObuFunctions/TrackAtlas/Simulation/FindStartOfNewSpeedProfile_TA_Lib_internal_dll.c
@@ -1,4 +1,4 @@
-const char*  pszDllPathname = "TA_Lib_internal__EVAL_Q_GDIR.dll";
+const char*  pszDllPathname = "TA_Lib_internal__FindStartOfNewSpeedProfile.dll";
 
 const char*  pszLauncherPathname = "C:/Program Files/Esterel Technologies/SCADE R16.1/SCADE/bin/SCSSMLNC.exe";
 
--- model/Scade/System/ObuFunctions/TrackAtlas/Simulation/TrackAtlas_newmapping.h
+++ model/Scade/System/ObuFunctions/TrackAtlas/Simulation/TrackAtlas_newmapping.h
@@ -1,10 +1,10 @@
 /* TrackAtlas_newmapping.h */
 
-#include "EVAL_Q_GDIR_TA_Lib_internal.h"
+#include "FindStartOfNewSpeedProfile_TA_Lib_internal.h"
 
 /*******************************
 * Simulation context
 *******************************/
-extern inC_EVAL_Q_GDIR_TA_Lib_internal inputs_ctx;
-extern outC_EVAL_Q_GDIR_TA_Lib_internal outputs_ctx;
+extern inC_FindStartOfNewSpeedProfile_TA_Lib_internal inputs_ctx;
+extern outC_FindStartOfNewSpeedProfile_TA_Lib_internal outputs_ctx;
 
