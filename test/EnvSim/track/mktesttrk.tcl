3e89d607ac49c2dc86d9e71493526a1215fd426f
Johannes Kastner <Johannes.Kastner@ics-ag.de>
2015-12-01 08:52:02 +0100

    EnvSim: update test.trk

--- test/EnvSim/track/mktesttrk.tcl
+++ test/EnvSim/track/mktesttrk.tcl
@@ -7,7 +7,8 @@ edit::addBG 5 1 900 0 {nid_packet 45 q_dir 1 nid_nm 2114079}
 edit::addBG 10 1 901 1 {nid_packet 42 q_dir 1 q_rbc 1 nid_c 426 nid_rbc 1 nid_radio 500 q_sleepsession 0}
 
 # BG 902 (conditional leveltrans + national values)
-edit::addBG 15 1 902 1 {nid_packet 46 q_dir 1 n_iter 1 m_leveltr(0) 1 nid_ntc(0) 1 m_leveltr(1) 3} {
+#edit::addBG 15 1 902 1 {nid_packet 46 q_dir 1 n_iter 1 m_leveltr(0) 1 nid_ntc(0) 1 m_leveltr(1) 3} {
+edit::addBG 15 1 902 1 {
   nid_packet     3
   q_dir          1
   q_scale        1
@@ -49,12 +50,9 @@ edit::addRMSG 20 {nid_message 3 m_ack 1 nid_lrbg 42600902} {
   q_dir        1
   q_scale      1
   d_leveltr    20
-  n_iter       1
+  n_iter       0
   m_leveltr(0) 3
   l_ackleveltr(0) 200
-  m_leveltr(1) 1
-  nid_ntc(1)   1
-  l_ackleveltr(1) 200
 } {
   nid_packet           5
   q_dir                1
@@ -73,6 +71,17 @@ edit::addRMSG 20 {nid_message 3 m_ack 1 nid_lrbg 42600902} {
   q_linkreaction(1)    0
   q_locacc(1)          2
 } {
+  nid_packet    21
+  q_dir         1
+  q_scale       1
+  n_iter        1
+  d_gradient(0) 0
+  q_gdir(0)     0
+  g_a(0)        0
+  d_gradient(1) 50000
+  q_gdir(1)     0
+  g_a(1)        255
+} {
   nid_packet  27
   q_dir       1
   q_scale     1
@@ -85,13 +94,20 @@ edit::addRMSG 20 {nid_message 3 m_ack 1 nid_lrbg 42600902} {
   v_static(1) 40
   q_front(1)  0
   n_iter(1)   0
-
 }
 
 # BG 903
 edit::addBG 30 1 903 1
 
 # BG 904
-edit::addBG 40 1 904 1
+edit::addBG 40 1 904 1 {
+  nid_packet   41
+  q_dir        1
+  q_scale      1
+  d_leveltr    0
+  n_iter       0
+  m_leveltr(0) 3
+  l_ackleveltr(0) 200
+} 
 
 ctrl::loadTree
