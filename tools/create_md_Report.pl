313ac3d407a0f16898eef934026dc7058822977f
Dragomir1987 <nikolic.dragomir1987@gmail.com>
2015-12-01 14:06:06 +0100

    updated create_md_Report and created report_151201_02.md

--- tools/create_md_Report.pl
+++ tools/create_md_Report.pl
@@ -24,87 +24,89 @@ print "done";
 
 sub write_report {
 	my $filename = 'report.md';
-	
-	
+
+
 	open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
 	print $fh "These are the weekly results of the **issue tracker**\n";
 	print $fh "============\n";
 	print $fh "\n";
-	print $fh "The following issues are open for the current week:\n";
+	print $fh "## Open Issues\n";
+	print $fh "### The following issues are open for the current week:\n";
 	print $fh "\n";
-	
+
 	my $textfile="Week_1.txt";
 	$fh=appened_reports($fh, $textfile);
-	
+
 	print $fh "\n";
 	print $fh "\n";
-	
+
 	print $fh "\n";
 	print $fh "\n";
 	print $fh "\n";
 	print $fh "\n";
 	print $fh "\n";
-	print $fh "Open issues of the second week:\n";
+	print $fh "### Open issues of the second week:\n";
 	print $fh "\n";
-	
+
 	$textfile="Week_2.txt";
 	$fh=appened_reports($fh, $textfile);
-	
-	
+
+
 	print $fh "\n";
 	print $fh "\n";
-	print $fh "Open issues of the third week:\n";
+	print $fh "### Open issues of the third week:\n";
 	print $fh "\n";
-	
+
 	$textfile="Week_3.txt";
 	$fh=appened_reports($fh, $textfile);
-	
+
 	#adding closed issues
 	print $fh "\n";
-	print $fh "The following issues have been closed for the current week:\n";
+	print $fh "## Closed Issues\n"
+	print $fh "### The following issues have been closed for the current week:\n";
 	print $fh "\n";
-	
+
 	$textfile="Week_1_closed_issues.txt";
 	$fh=appened_reports($fh, $textfile);
-	
+
 	print $fh "\n";
 	print $fh "\n";
-	
+
 	print $fh "\n";
 	print $fh "\n";
 	print $fh "\n";
 	print $fh "\n";
 	print $fh "\n";
-	print $fh "Closed issues of the second week:\n";
+	print $fh "### Closed issues of the second week:\n";
 	print $fh "\n";
-	
+
 	$textfile="Week_2_closed_issues.txt";
 	$fh=appened_reports($fh, $textfile);
-	
-	
+
+
 	print $fh "\n";
 	print $fh "\n";
-	print $fh "Closed issues of the third week:\n";
+	print $fh "### Closed issues of the third week:\n";
 	print $fh "\n";
-	
+
 	$textfile="Week_3_closed_issues.txt";
 	$fh=appened_reports($fh, $textfile);
-	
+
 	#stop adding closed issues
-	
-	
+
+
 	print $fh "\n";
 	print $fh "\n";
 	print $fh "The results of the test report are:\n";
 	print $fh "============";
-	
-	
+
+
 	$textfile="../test/TestReport_Text.txt";
 	$fh=appened_reports($fh, $textfile);
-	
+
 	close $fh;
-	
-	
+
+
 	#remove old reports
 	unlink 'Week_1.txt';
 	unlink 'Week_2.txt';
@@ -120,15 +122,15 @@ sub appened_reports {
 	my @list=@_;
 	my $fh=$list[0];
 	my $filename=$list[1];
-	
+
 	my $dir_1 = dir(".");
 	my $file_1 = $dir_1->file($filename);
 	my $content_1= $file_1->slurp();
 	my $file_handle_1 = $file_1->openr();
-	
+
 	while( my $line = $file_handle_1->getline() ) {
         print $fh $line;
         }
-	
+
 	return $fh;
 }
