faa9c089c3dc9b9dab85ddff7b5eefa53d8e8a3a
Stefan Karg <info@stefankarg-it.de>
2015-12-08 16:31:42 +0100

    Modified buildscript for libenvsim elf_x64 to use litclstub8.6

--- test/EnvSim/libenvsim/makex64.sh
+++ test/EnvSim/libenvsim/makex64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CFLAGS="-m64 -I/usr/include/tcl8.5/"
+CFLAGS="-m64 -I/usr/include/tcl/"
 
 mkdir -p build/elf_x64
 cd build/elf_x64
-cmake -D CMAKE_C_FLAGS="$CFLAGS" -D ENABLE_SCADE=true -D WITH_TCL_EXTENSION=true -D ENABLE_TESTS=false -DJIMTCL=$(pwd)/../../jimtcl ../../
+cmake -D CMAKE_C_FLAGS="$CFLAGS" -D ENABLE_SCADE=true -D WITH_TCL_EXTENSION=true-D TCLSTUB=tclstub8.6 -D ENABLE_TESTS=false -DJIMTCL=$(pwd)/../../jimtcl ../../
 make 
 
 cp libenvsim.a ../../dist/elf_x64/
