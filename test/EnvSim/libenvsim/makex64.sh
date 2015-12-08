bd8170a697434000cdc757f89e983d8ea9b35c01
Stefan Karg <info@stefankarg-it.de>
2015-12-08 14:39:23 +0100

    Enable TCL option in libenvsim for x64 architecture

--- test/EnvSim/libenvsim/makex64.sh
+++ test/EnvSim/libenvsim/makex64.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-CFLAGS="-m64"
+CFLAGS="-m64 -I/usr/include/tcl8.5/"
 
 mkdir -p build/elf_x64
 cd build/elf_x64
-cmake -D CMAKE_C_FLAGS="$CFLAGS" -D ENABLE_SCADE=true -D WITH_TCL_EXTENSION=false -D ENABLE_TESTS=false -DJIMTCL=$(pwd)/../../jimtcl ../../
+cmake -D CMAKE_C_FLAGS="$CFLAGS" -D ENABLE_SCADE=true -D WITH_TCL_EXTENSION=true -D ENABLE_TESTS=false -DJIMTCL=$(pwd)/../../jimtcl ../../
 make 
 
 cp libenvsim.a ../../dist/elf_x64/
+cp envsim.so ../../dist/elf_x64/
 
 VERSION=$(cat ../../CMakeLists.txt | grep project | cut -d' ' -f 3)
 echo -e "$VERSION\n$(date)" > ../../dist/elf_x64/VERSION
