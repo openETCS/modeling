e3c3d779ac7e1e9bd04172910f74e13404c4bd83
Johannes Kastner <Johannes.Kastner@ics-ag.de>
2015-12-13 19:35:49 +0100

    libenvsim 0.6.1

--- test/EnvSim/libenvsim/src/scade/scade_probe.c
+++ test/EnvSim/libenvsim/src/scade/scade_probe.c
@@ -5,12 +5,17 @@
 // History:
 // - 28.10.15, J. Kastner: initial version
 // - 30.10.15, J. Kastner: add handling of train messages to trackside_cycle()
+// - 13.12.15, J. Kastner: add sending of braking curves data
 
 #include <stdio.h>
 #include "../tcp.h"
 #include "ProbeTracksideInput_EnvSim.h"
 #include "ProbeSDM_EnvSim.h"
 #include "ProbeEVC_EnvSim.h"
+
+// maximum nunber of arcs in ParabolaCurve_T
+#define MAX_BC_ARCS 114
+#define MAX_MRSP_SECTIONS 110
 
 typedef struct {
   kcg_bool valid;
@@ -20,9 +25,9 @@ typedef struct {
 } es_scade_probe_CurveSeg;
 
 
-typedef struct {
-  es_scade_probe_CurveSeg eoa_sbd[2];
-} es_scade_probe_BrakingCurves;
+//typedef struct {
+//  es_scade_probe_CurveSeg eoa_sbd[2];
+//} es_scade_probe_BrakingCurves;
 
 
 const size_t PROBE_TRACKSIDE_BMSG_SIZE = sizeof(CompressedBaliseMessage_TM);
@@ -32,7 +37,8 @@ const size_t PROBE_SDM_TARGET_SIZE = sizeof(Target_T_TargetManagement_types);
 const size_t PROBE_EVC_MORC_REG_SIZE = sizeof(mobileRegistrationContext_T_RCM_Types_Pkg);
 const size_t PROBE_EVC_MORC_CON_SIZE = sizeof(mobileConnectionContext_T_RCM_Types_Pkg);
 const size_t PROBE_EVC_MORC_SES_SIZE = sizeof(sessionStatus_T_RCM_Session_Types_Pkg);
-const size_t PROBE_EVT_BC_SIZE = sizeof(es_scade_probe_BrakingCurves);
+const size_t PROBE_PARABOLACURVE_SIZE = sizeof(ParabolaCurve_T_CalcBrakingCurves_types);
+//const size_t PROBE_EVT_BC_SIZE = sizeof(es_scade_probe_BrakingCurves);
 
 
 // If not NULL, send all events (track messages, train messages) to this stream
@@ -82,41 +88,108 @@ void es_scade_probe_sdm_init(outC_ProbeSDM_EnvSim *outC) {
 
 }
 
+void es_scade_probe_addcurve(char** buf, ParabolaCurve_T_CalcBrakingCurves_types *c, int *rest, kcg_real* max_dist) {
+  int i, len;
+  int r = *rest;
+  char *p = *buf;
+  kcg_real last_dist = -1;
+  for (i = 0; i < MAX_BC_ARCS; i++) {
+    if (c->valid[i] && c->distances[i] != last_dist) {
+      if( c->distances[i] > *max_dist ) {
+        *max_dist = c->distances[i];
+      }
+      len = snprintf(p, r, "%f %f %f ", c->distances[i], c->accelerations[i], c->speeds[i]);
+      r -= len;
+      p += len;
+    }
+    last_dist = c->distances[i];
+  }
+  *rest = r;
+  *buf = p;
+}
 
 void es_scade_probe_curves(CurveCollection_T_CalcBrakingCurves_types *curves) {
-  static int cycle = 0;
-  static es_scade_probe_BrakingCurves data;
-  static uint16_t f16_eoasbd = 0;
+  static uint16_t last_f16 = 0;
 
-  if( cycle % 10 == 0 ) {
+  ParabolaCurve_T_CalcBrakingCurves_types *eoasbd = &curves->EOA_SBD_curve;
+  ParabolaCurve_T_CalcBrakingCurves_types *svlebd = &curves->SvL_LoA_EBD_curve;
+//  ParabolaCurve_T_CalcBrakingCurves_types *gui = &curves->GUI_curve;
+  ParabolaCurve_T_CalcBrakingCurves_types *mrsp = curves->MRSP_EBD_curves;
+  uint16_t f16 = fletcher16((uint8_t*)eoasbd, PROBE_PARABOLACURVE_SIZE)
+               + fletcher16((uint8_t*)svlebd, PROBE_PARABOLACURVE_SIZE)
+               + fletcher16((uint8_t*)mrsp, MAX_MRSP_SECTIONS * PROBE_PARABOLACURVE_SIZE);
+
+  if( f16 != last_f16 ) {
     char buf[4000];
     int rest = 4000;
     char *p = buf;
     int i, len;
+    kcg_real max_dist = 0;
 
-    ParabolaCurve_T_CalcBrakingCurves_types *c = &curves->EOA_SBD_curve;
+    /* EOA-SBD curve */
+    if( eoasbd->valid[0] ) {
 
-    len = snprintf(p,rest,"eoasbd {");
-    rest -= len;
-    p += len;
+      len = snprintf(p, rest, "eoasbd {");
+      rest -= len;
+      p += len;
 
-    kcg_real last_dist = -1;
-    for(i=0; i<10; i++) {
-      if( c->valid[i] && c->distances[i] != last_dist ) {
-        len = snprintf(p,rest,"%f %f %f ", c->distances[i], c->accelerations[i], c->speeds[i]);
+      es_scade_probe_addcurve(&p, eoasbd, &rest, &max_dist);
+      len = snprintf(p, rest, "} ");
+      rest -= len;
+      p += len;
+    }
+
+    /* SvL-EBD curve */
+    if( svlebd->valid[0] ) {
+      len = snprintf(p, rest, "svlebd {");
+      rest -= len;
+      p += len;
+
+      es_scade_probe_addcurve(&p, svlebd, &rest, &max_dist);
+      len = snprintf(p, rest, "} ");
+      rest -= len;
+      p += len;
+    }
+
+//    /* GUI curve */
+//    if( gui->valid[0] ) {
+//      len = snprintf(p, rest, "gui {");
+//      rest -= len;
+//      p += len;
+//
+//      es_scade_probe_addcurve(&p, gui, &rest, &max_dist);
+//      len = snprintf(p, rest, "} ");
+//      rest -= len;
+//      p += len;
+//    }
+
+    kcg_bool hasMrsp = false;
+    for(i=0; i<MAX_MRSP_SECTIONS; i++) {
+      if(!mrsp[i].valid[0])
+        continue;
+
+      if(!hasMrsp) {
+        len = snprintf(p, rest, "mrsp {");
         rest -= len;
         p += len;
-        last_dist = c->distances[i];
+        hasMrsp = true;
       }
 
+      es_scade_probe_addcurve(&p, &mrsp[i], &rest, &max_dist);
     }
-    snprintf(p,rest,"}");
 
-    es_tcp_send(scade_probe_evtstream,TCPMSG_ES_EVT_BC,(char*)buf,strlen(buf));
+    if( hasMrsp ) {
+      len = snprintf(p, rest, "} ");
+      rest -= len;
+      p += len;
+    }
+
+    snprintf(p, rest, "max_dist %f",max_dist);
+
+    es_tcp_send(scade_probe_evtstream, TCPMSG_ES_EVT_BC, (char *) buf, strlen(buf));
   }
 
-
-  cycle++;
+  last_f16 = f16;
 }
 
 void es_scade_probe_sdm_cycle(TargetCollection_T_TargetManagement_types *targetCollection,
