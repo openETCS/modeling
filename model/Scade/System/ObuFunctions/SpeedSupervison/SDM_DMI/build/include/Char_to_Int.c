020f9d58a4983517dd0dff9115d00c027bd86234
Thorsten Schulz (Uni Rostock) <T12z@users.noreply.github.com>
2016-06-20 16:06:54 +0200

    OBU/SDM/DMI: cleanup build-system

--- model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/include/Char_to_Int.c
+++
@@ -1,14 +0,0 @@
-#include "kcg_types.h"
-
- 
-kcg_int Char_to_Int_Utilities(kcg_char char_out)
-
-{
-
-kcg_int int_in=(kcg_int) char_out;
-
-return  int_in;	
-} 
-    
- 
-  
--- model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/include/kcg_assign.h
+++
@@ -1,4 +0,0 @@
-#ifndef kcg_assign
-#include "string.h"
-#define kcg_assign(kcg_C1, kcg_C2, kcg_size) (memcpy((kcg_C1), (kcg_C2), (kcg_size)))
-#endif /* kcg_assign */
