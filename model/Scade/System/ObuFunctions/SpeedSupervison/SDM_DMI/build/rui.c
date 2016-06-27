8de3c3c2d6a3bcd85fb74ed960a270cda55f9a89
Thorsten Schulz (Uni Rostock) <T12z@users.noreply.github.com>
2016-06-27 16:52:25 +0200

    OBU/SDM/DMI: sync udp library with other projects; +CDT setup

--- model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/rui.c
+++ model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/rui.c
@@ -10,12 +10,22 @@
 #define MAX(a,b) (((a)>(b))?(a):(b))
 #define MIN(a,b) (((a)>(b))?(b):(a))
 
+#ifdef _RM57Lx_
+
+#include "lwip/sys.h"
+
+#define close(fd) udp_remove(fd)
+void RUI_capture(void *arg, struct udp_pcb *pcb, struct pbuf *p, ip_addr_t *addr, u16_t port);
+
+#endif
+
 typedef struct rui_context {
 	rawUdpIn *rui;
 	int ruis;
 	int defaultFailureCredits;
 	int failureCredits;
 	int64_t readMask;
+	int64_t answMask;
 	long timeoutMS;
 	int idleInterval;
 	rui_timeout notify;
@@ -24,7 +34,7 @@ typedef struct rui_context {
 #ifdef WINDOWS
 	LARGE_INTEGER lastTime;
 #elif defined(_RM57Lx_)
-
+	bool triggered;
 #else
 	struct timeval lastTime;
 #endif
@@ -53,6 +63,7 @@ bool RUI_init(rawUdpIn *rui, const uint16_t *lport, const uint16_t *dport, int r
 	rctx.defaultFailureCredits = failureCredits;
 	rctx.failureCredits = rctx.defaultFailureCredits;
 	rctx.readMask = 0;
+	rctx.answMask = 0;
 	rctx.rui = rui;
 	rctx.ruis = ruis;
 	rctx.timeoutMS = timeout;
@@ -82,53 +93,44 @@ bool RUI_init(rawUdpIn *rui, const uint16_t *lport, const uint16_t *dport, int r
 	if (gettimeofday(&rctx.lastTime, NULL)) { rctx.ruis = 0; return false; }
 #endif
 	memset(rui, 0, sizeof(rawUdpIn)*ruis);
-	char em[32] = "(0) ";
+	char em[32] = "(00) ";
 	for (int i=0; (i<rctx.ruis)&&rctx.failureCredits; i++) {
 
-		rctx.rui[i].addr.sin_family = AF_INET;
-		rctx.rui[i].fd = socket(rctx.rui[i].addr.sin_family, SOCK_DGRAM, 0);
+		rctx.rui[i].initiative = dport[i];
+
+#ifdef _RM57Lx_
+#if IP_SOF_BROADCAST || SO_REUSE
+#error setting options BROADCAST and REUSE is not implemented
+#endif
+		rctx.rui[i].fd = udp_new();
+	    udp_recv(rctx.rui[i].fd, RUI_capture, (void *)i);
+	    udp_bind(rctx.rui[i].fd, IP_ADDR_ANY, lport[i]);
+
+#else
+		rctx.rui[i].fd = socket(AF_INET, SOCK_DGRAM, 0);
 		if (rctx.rui[i].fd < 0) strcpy(em+4, "ERROR opening socket"),RUI_error(em);
-		rctx.rui[i].initiative = htons(dport[i]);
 		int optval = 1;
 		setsockopt(rctx.rui[i].fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
 		if (rctx.rui[i].initiative)
 			setsockopt(rctx.rui[i].fd, SOL_SOCKET, SO_BROADCAST, (const void *)&optval , sizeof(int));
-
 		struct sockaddr_in InAddr = {
-				.sin_family = rctx.rui[i].addr.sin_family,
+				.sin_family = AF_INET,
 				.sin_port = htons(lport[i]),
 				.sin_addr = { .s_addr = localhost ? htonl(INADDR_LOOPBACK) :  htonl(INADDR_ANY) }
 		};
-		if (bind(rctx.rui[i].fd, (struct sockaddr *) &InAddr, sizeof(struct sockaddr_in)) < 0) strcpy(em+4, "binding Listener"),RUI_error(em);
-		RUI_invalidate(i);
-		em[1]++;
+		if (bind(rctx.rui[i].fd, (struct sockaddr *) &InAddr, sizeof(struct sockaddr_in)) < 0) strcpy(em+5, "binding Listener"),RUI_error(em);
+#endif
+		if (++em[2] == ('0'+10)) em[1]++, em[2]-=10;
 	}
 	return true;
 }
 
-void RUI_invalidate(int i) {
-	if (i < rctx.ruis) rctx.rui[i].length = -1;
-}
-
 void RUI_fill(int i, bool push, void *buf, ssize_t len) {
 	if (i < rctx.ruis) {
-		if ((push || RUI_isReadable(i)) && (RUI_isKnown(i) || rctx.rui[i].initiative)) {
-			if (!RUI_isKnown(i) && rctx.sparsePing) len = 0;
-			rctx.rui[i].length = MIN(sizeof(rctx.rui[i].buf),len);
-			if (rctx.rui[i].length > 0) {
-				if (buf) memcpy(rctx.rui[i].buf, buf, rctx.rui[i].length);
-				else RUI_invalidate(i);
-			}
-		} else RUI_invalidate(i);
-	}
-}
-
-void RUI_fake(int i, void *buf, ssize_t len, uint16_t port) {
-	if (i < rctx.ruis) {
-		rctx.rui[i].addr.sin_port = htons(port);
-		rctx.rui[i].addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-		rctx.readMask |= (1 << i);
-		RUI_fill(i, true, buf, len);
+		if ((push || RUI_wasReadable(i)) && (RUI_isKnown(i) || rctx.rui[i].initiative)) {
+			if ((!buf && len > 0) || (!RUI_isKnown(i) && rctx.sparsePing)) len = 0;
+			rui_send(i, buf, len);
+		}
 	}
 }
 
@@ -136,11 +138,53 @@ bool RUI_isReadable(int i) {
 	return (i < rctx.ruis && (rctx.readMask & (1 << i)));
 }
 
+bool RUI_wasReadable(int i) {
+	return (i < rctx.ruis && (rctx.answMask & (1 << i)));
+}
+
 ssize_t RUI_get(int i, void *buf, ssize_t len) {
-	size_t l = -1;
+	ssize_t l = -1;
 	if (RUI_isReadable(i) && len >= 0) {
-		l = MIN(rctx.rui[i].length, len);
-		memcpy(buf, rctx.rui[i].buf, l);
+
+#ifdef _RM57Lx_
+	    SYS_ARCH_DECL_PROTECT(old_level);
+	    SYS_ARCH_PROTECT(old_level);
+
+		if (rctx.rui[i].p) {
+			struct pbuf *p = rctx.rui[i].p;
+			rctx.rui[i].p = 0;
+			rctx.rui[i].sport = rctx.rui[i].pport;
+			rctx.rui[i].saddr = rctx.rui[i].paddr;
+		    SYS_ARCH_UNPROTECT(old_level);
+
+			l = MIN(p->tot_len, len);
+			pbuf_copy_partial(p, buf, l, 0);
+
+			pbuf_free(p);
+		} else
+		    SYS_ARCH_UNPROTECT(old_level);
+
+#else
+
+		struct sockaddr_in s_in;
+		uint32_t lAddr = sizeof(s_in);
+		l = recvfrom(rctx.rui[i].fd, buf, len, 0, (struct sockaddr *)&s_in, &lAddr);
+		if (l >= 0) {
+			rctx.rui[i].saddr.addr = ntohl(s_in.sin_addr.s_addr);
+			rctx.rui[i].sport = ntohs(s_in.sin_port);
+
+/*			if (verbose && slen) {
+				char ipStr[INET6_ADDRSTRLEN];
+				if (ipStr != inet_ntop(AF_INET, &s_in.sin_addr, ipStr, sizeof(ipStr))) RUI_error("inet_ntop");
+				snprintf(verbose, slen, "readRaw(%ld) from %s:%d to [%d/%d]", l, ipStr, rctx.rui[r].sport, r, rctx.ruis);
+			}
+*/
+		} else
+			RUI_error("recv short message");
+
+		rctx.readMask &= ~(1 << i);
+#endif
+
 	}
 	return l;
 }
@@ -152,7 +196,7 @@ bool RUI_isRunnable() {
 }
 
 bool RUI_isKnown(int i) {
-	return (i < rctx.ruis) && (!!rctx.rui[i].addr.sin_addr.s_addr);
+	return (i < rctx.ruis) && rctx.rui[i].saddr.addr;
 }
 
 void RUI_close() {
@@ -162,54 +206,53 @@ void RUI_close() {
 	rctx.ruis = 0;
 }
 
-int64_t RUI_read(char *verbose, ssize_t len) {
-	for(int r=0; r<rctx.ruis; r++) if (rctx.readMask & (1 << r)) {
-		rawUdpIn ruil;
-		socklen_t lAddr = sizeof(struct sockaddr_in);
-		ruil.length = recvfrom(rctx.rui[r].fd, (void *)ruil.buf, sizeof(ruil.buf), 0, (struct sockaddr *)&ruil.addr, &lAddr);
-		if (ruil.length >= 0) {
-			rctx.rui[r].rxCnt++;
-			char ipStr[INET6_ADDRSTRLEN];
-			if (ipStr != inet_ntop(ruil.addr.sin_family, &ruil.addr.sin_addr, ipStr, sizeof(ipStr))) RUI_error("inet_ntop");
+#ifdef _RM57Lx_
+void RUI_capture(void *arg, struct udp_pcb *pcb, struct pbuf *p, ip_addr_t *addr, u16_t port) {
+	int r = (int)arg;
 
-			rctx.rui[r].addr = ruil.addr;
-			rctx.rui[r].length = ruil.length;
-			memcpy(rctx.rui[r].buf, ruil.buf, ruil.length);
-
-			if (verbose && len)
-				snprintf(verbose, len, "readRaw(%ld) from %s:%d to [%d/%d]", ruil.length, ipStr, ntohs(ruil.addr.sin_port), r, rctx.ruis);
-
-		} else {
-			rctx.readMask &= ~(1 << r);
-			RUI_error("recv short message");
-		}
-	}
-	return rctx.readMask;
+	SYS_ARCH_DECL_PROTECT(old_level);
+    SYS_ARCH_PROTECT(old_level);
+		struct pbuf *old = rctx.rui[r].p;
+		rctx.rui[r].p = p;
+		rctx.rui[r].pport = port;
+		rctx.rui[r].paddr.addr = ntohl(addr->addr);
+		rctx.rui[r].rxCnt++;
+		rctx.readMask |= 1 << r;
+    SYS_ARCH_UNPROTECT(old_level);
+	if (old) pbuf_free(old);
 }
+#endif
 
-bool RUI_send() {
-	for (int n=0; n<rctx.ruis; n++)
-		if (rctx.rui[n].length >= 0) {
-			int flags = 0;
-			if (!rctx.rui[n].addr.sin_addr.s_addr && rctx.rui[n].initiative) {
-				rctx.rui[n].addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-				rctx.rui[n].addr.sin_port = rctx.rui[n].initiative;
-			}
-			else flags = MSG_CONFIRM;
+void rui_send(int i, void *buf, ssize_t len) {
+	if (len >= 0) {
+#ifdef _RM57Lx_
+		struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, len, PBUF_RAM);
+		if (!p) return;
+		if (buf) memcpy(p->payload, buf, len);
+		if (!rctx.rui[i].saddr.addr && rctx.rui[i].initiative) {
+			ip_addr_t bca = { .addr = INADDR_BROADCAST };
+			udp_sendto(rctx.rui[i].fd, p, &bca, rctx.rui[i].initiative);
+		} else
+			udp_sendto(rctx.rui[i].fd, p, &rctx.rui[i].saddr, rctx.rui[i].sport);
+		pbuf_free(p);
 
-			int l=0;
-			if (rctx.rui[n].addr.sin_addr.s_addr) {
-				l = sendto(rctx.rui[n].fd, (void *)rctx.rui[n].buf, rctx.rui[n].length, flags, (struct sockaddr *) &rctx.rui[n].addr, sizeof(rctx.rui[n].addr));
-				if (rctx.rui[n].addr.sin_addr.s_addr == htonl(INADDR_BROADCAST)) rctx.rui[n].addr.sin_addr.s_addr = 0;
-			}
-
-			rctx.rui[n].length = -1; // make same buffer unsendable
-			if (l < 0) {
-				RUI_error("sendto failed on sendRaw");
-				return false;
-			}
+#else
+		int flags = 0;
+		struct sockaddr_in s_in = { .sin_family = AF_INET };
+		if (!rctx.rui[i].saddr.addr && rctx.rui[i].initiative) {
+			s_in.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+			s_in.sin_port =        htons(rctx.rui[i].initiative);
+		} else {
+			s_in.sin_addr.s_addr = htonl(rctx.rui[i].saddr.addr);
+			s_in.sin_port =        htons(rctx.rui[i].sport);
+			flags = MSG_CONFIRM;
 		}
-	return true;
+
+		if (s_in.sin_addr.s_addr)
+			if (sendto(rctx.rui[i].fd, buf, len, flags, (struct sockaddr *) &s_in, sizeof(s_in)) < 0)
+				RUI_error("sendto failed on sendRaw");
+#endif
+	}
 }
 
 void RUI_status() {
@@ -234,72 +277,108 @@ void RUI_status() {
 	}
 }
 
+#ifdef _RM57Lx_
+
+void RUI_trigger() {
+	SYS_ARCH_DECL_PROTECT(old_level);
+	SYS_ARCH_PROTECT(old_level);
+
+	rctx.triggered = true;
+	SYS_ARCH_UNPROTECT(old_level);
+
+}
+
 bool RUI_poll() {
-	static int pollret = 0;
-	RUI_send();
-	RUI_status();
+	SYS_ARCH_DECL_PROTECT(old_level);
+	SYS_ARCH_PROTECT(old_level);
 
-	fd_set rfds;
-	int nfds = 0;
-	FD_ZERO(&rfds);
-	long tdiff = 0;
+	if (rctx.triggered) {
+		rctx.triggered = false;
+		rctx.answMask = rctx.readMask;
+		if (!rctx.readMask) {
+			SYS_ARCH_UNPROTECT(old_level);
+			if (rctx.notify) rctx.notify();
+		} else {
+			SYS_ARCH_UNPROTECT(old_level);
+			rctx.failureCredits = rctx.defaultFailureCredits;
+		}
 
+		// MODEL_run();
+
+		RUI_status();
+		return true;
+	} else {
+		SYS_ARCH_UNPROTECT(old_level);
+		return false;
+	}
+}
+
+#else
+
+void RUI_delay(int tMS) {
 #ifdef WINDOWS
-	tdiff = -rctx.lastTime.QuadPart;
+	long tdiff = 0-rctx.lastTime.QuadPart;
 	QueryPerformanceCounter(&rctx.lastTime);
 	tdiff += rctx.lastTime.QuadPart;
 	LARGE_INTEGER Frequency;
 	QueryPerformanceFrequency(&Frequency);
 	tdiff *= 1000000;
 	tdiff /= Frequency.QuadPart;
-	tdiff = 1000*rctx.timeoutMS - tdiff;
+	tdiff = 1000*tMS - tdiff;
 	if (tdiff > 0) {
 		usleep(tdiff);
 		rctx.lastTime.QuadPart += tdiff;
 //		QueryPerformanceCounter(&rctx.lastTime);
 	}
-#elif defined (_RM57Lx_)
 
 #else
 
-	tdiff = -(rctx.lastTime.tv_sec*1000000L+rctx.lastTime.tv_usec);
+	long tdiff = 0-(rctx.lastTime.tv_sec*1000000L+rctx.lastTime.tv_usec);
 	gettimeofday(&rctx.lastTime, NULL);
 	tdiff += (rctx.lastTime.tv_sec*1000000L+rctx.lastTime.tv_usec);
-	tdiff = 1000*rctx.timeoutMS - tdiff;
+	tdiff = 1000*tMS - tdiff;
 	if (tdiff > 0) {
 		usleep(tdiff);
 		rctx.lastTime.tv_usec += tdiff;
 //		gettimeofday(&rctx.lastTime, NULL);
 	}
 #endif
+}
+
+bool RUI_poll() {
+
+	RUI_delay(rctx.timeoutMS);
+
+	fd_set rfds;
+	int nfds = 0;
+	FD_ZERO(&rfds);
 
 	for (int i=0; i<rctx.ruis; i++) {
 		FD_SET(rctx.rui[i].fd, &rfds);
 		if (rctx.rui[i].fd > nfds) nfds = rctx.rui[i].fd+1;
 	}
 
+	rctx.readMask = 0;
 	errno = 0;
 	struct timeval tv = {0, 0};
-	rctx.readMask = 0;
-	pollret = select(nfds, &rfds, NULL, NULL, &tv);  // check all sockets
-	switch (pollret) {
-		case -1:
-			if ((errno != EINTR) || (errno != EAGAIN)) {
-				RUI_error("select failed");
-				return false;
-			} else
-				break;
-		case 0: /* that's a timeout */
+	if (select(nfds, &rfds, NULL, NULL, &tv) < 0) {
+		if ((errno != EINTR) || (errno != EAGAIN)) {
+			RUI_error("select failed");
+			return false;
+		}
+	} else {
+		for (int i=0; i<rctx.ruis; i++) if (FD_ISSET(rctx.rui[i].fd, &rfds)) {
+			rctx.readMask |= 1 << i;
+			rctx.rui[i].rxCnt++;
+		}
+		rctx.answMask = rctx.readMask;
+		if (!rctx.readMask) {
 			if (rctx.notify) rctx.notify();
-			break;
-		default:
-			for (int i=0; i<rctx.ruis; i++) if (FD_ISSET(rctx.rui[i].fd, &rfds)) rctx.readMask |= 1 << i;
-			if (!RUI_read(0/*str*/, 0 /*sizeof(str)*/))
-				RUI_error("nothing read. Probably an OUT-socket died");
-			else
-				rctx.failureCredits = rctx.defaultFailureCredits;
-			break;
+		} else
+			rctx.failureCredits = rctx.defaultFailureCredits;
 	}
+	RUI_status();
 	return true;
 
 }
+#endif
--- model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/rui.h
+++ model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/rui.h
@@ -18,28 +18,39 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
-#include <sys/time.h>
 #include <errno.h>
 
 #ifdef WINDOWS
 #undef  _WIN32_WINNT
 #define _WIN32_WINNT 0x0600
 
+#include <unistd.h>
 #include <Winsock2.h>
 #include <Ws2tcpip.h>
+#include <sys/time.h>
 #include <time.h>
-typedef int socklen_t;
-typedef uint16_t in_port_t;
+
+struct ip_addr {
+	uint32_t addr;
+};
 
 #elif defined(_RM57Lx_)
 
 #include "utils/lwiplib.h"
+#include "lwip/inet.h"
+
+typedef int ssize_t;
+extern void RUI_trigger();
 
 #else
-	
+struct ip_addr {
+	uint32_t addr;
+};
+
+#include <unistd.h>
+#include <sys/time.h>
 #include <sys/select.h>
 #include <arpa/inet.h>
 
@@ -55,29 +66,34 @@ typedef void (*rui_timeout)(void);
 typedef void (*rui_puts)(char *);
 
 typedef struct rawUdpIn {
-	struct sockaddr_in addr;
-	int fd;
-	in_port_t initiative;
+	struct ip_addr saddr;
+	uint16_t       sport;  /*host-order */
+	uint16_t  initiative;  /*host-order */
 	int rxCnt;
-	ssize_t length;
-	uint8_t buf[BUFSIZE];
+
+#if defined(_RM57Lx_)
+	struct udp_pcb *fd;
+	struct pbuf   *p;
+	struct ip_addr paddr;  /*host-order */
+	uint16_t       pport;  /*host-order */
+#else
+	int fd;
+#endif
 } rawUdpIn;
 
 void RUI_error(char *msg);
 int64_t RUI_getCurrentMask();
 bool RUI_init(rawUdpIn *rui, const uint16_t *lport, const uint16_t *dport, int ruis, bool localhost, bool sparsePing, int failureCredits, int timeout, bool status, rui_timeout notify, rui_puts dputs);
-void RUI_invalidate(int i);
 void RUI_fill(int i, bool push, void *buf, ssize_t len);
-void RUI_fake(int i, void *buf, ssize_t len, uint16_t port);
 bool RUI_isReadable(int i);
+bool RUI_wasReadable();
 ssize_t RUI_get(int i, void *buf, ssize_t len);
 bool RUI_isRunnable();
 bool RUI_isKnown(int i);
 bool RUI_poll();
 
 void RUI_close();
-int64_t RUI_read(char *verbose, ssize_t len);
-bool RUI_send();
+void rui_send(int i, void *buf, ssize_t len);
 void RUI_status();
 
 #endif /* RAW_UDP_IN_H_ */
