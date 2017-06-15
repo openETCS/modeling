e0e70fef6127c8a8463468e744a81d17c05f237b
thorsten@ChickenJoe <thorsten.schulz@uni-rostock.de>
2017-06-15 18:24:13 +0200

    OBU/SDM: make gradients a bit more challenging in the TB EVC: update SDM integration op Demo: add generation target

--- model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/Make.bat
+++ model/Scade/System/ObuFunctions/SpeedSupervison/SDM_DMI/build/Make.bat
@@ -1,6 +1,6 @@
-@SET PATH=C:/Program Files (x86)/Esterel Technologies/SCADE R16.2.1/SCADE/../contrib/Msys/mingw_w64/bin;C:/Program Files (x86)/Esterel Technologies/SCADE R16.2.1/SCADE/../contrib/Msys/bin;%PATH%
-@SET INCLUDE=C:/Program Files (x86)/Esterel Technologies/SCADE R16.2.1/SCADE/../contrib/Msys/mingw_w64/include
-@SET LIB=C:/Program Files (x86)/Esterel Technologies/SCADE R16.2.1/SCADE/../contrib/Msys/mingw_w64/lib
+@SET PATH=C:/Program Files (x86)/Esterel Technologies/SCADE R17.2/SCADE/../contrib/Msys/mingw_w64/bin;C:/Program Files (x86)/Esterel Technologies/SCADE R17.2/SCADE/../contrib/Msys/bin;%PATH%
+@SET INCLUDE=C:/Program Files (x86)/Esterel Technologies/SCADE R17.2/SCADE/../contrib/Msys/mingw_w64/include
+@SET LIB=C:/Program Files (x86)/Esterel Technologies/SCADE R17.2/SCADE/../contrib/Msys/mingw_w64/lib
 @SET ARG=%1
 gcc -print-search-dirs
-@"C:/Program Files (x86)/Esterel Technologies/SCADE R16.2.1/SCADE/../contrib/Msys/mingw_w64/bin/mingw32-make.exe" -f Makefile %ARG%
+@"C:/Program Files (x86)/Esterel Technologies/SCADE R17.2/SCADE/../contrib/Msys/mingw_w64/bin/mingw32-make.exe" -f Makefile %ARG%
